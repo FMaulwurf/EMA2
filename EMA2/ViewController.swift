22714ff17b0e66ac9b4784e22ce0d917c1966bc0
Felix Maulwurf <s52403@beuth-hochschule.de>
2017-10-17 20:34:53 +0200

    init commit

---
+++ EMA2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EMA2
+//
+//  Created by Felix M. on 17.10.17.
+//  Copyright © 2017 Felix M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
