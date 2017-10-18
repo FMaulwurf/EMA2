2c3d9c270f5d29d9be5bf01ceb0c118d7388c5cf
Michael Duve <mduve@designmail.net>
2017-10-18 14:45:00 +0200

    removed charts framework. using progressView

--- EMA2/ViewController.swift
+++ EMA2/ViewController.swift
@@ -1,75 +1,64 @@
-//
-//  ViewController.swift
-//  EMA2
-//
-//  Created by Felix M. on 17.10.17.
-//  Copyright © 2017 Felix M. All rights reserved.
-//
-
 import UIKit
 import CoreMotion
 import Charts
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var xValue: UILabel!
-    @IBOutlet weak var yValue: UILabel!
-    @IBOutlet weak var zValue: UILabel!
+    @IBOutlet weak var xProgressView: UIProgressView!
+    @IBOutlet weak var yProgressView: UIProgressView!
+    @IBOutlet weak var zProgressView: UIProgressView!
     
-    @IBOutlet var barChartView: BarChartView!
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    let lengthOfDoubles = 6
+    let sampleSpeed = 0.05
     
     let motionManager = CMMotionManager()
     
-    var x : Double = 0.0
-    var y : Double = 0.0
-    var z : Double = 0.0
+    var x : Double = 0.0 {
+        didSet(x) {
+            let normalizedX: Double = normalizeValue(val: x)
+            self.xProgressView.setProgress(Float(normalizedX), animated: false)
+            self.xLabel.text = String(x.truncate(places: self.lengthOfDoubles))
+        }
+    }
+    var y : Double = 0.0 {
+        didSet(y) {
+            let normalizedY: Double = normalizeValue(val: y)
+            self.yProgressView.setProgress(Float(normalizedY), animated: false)
+            self.yLabel.text = String(y.truncate(places: self.lengthOfDoubles))
+        }
+    }
+    var z : Double = 0.0 {
+        didSet(z) {
+            let normalizedZ: Double = normalizeValue(val: z)
+            self.zProgressView.setProgress(Float(normalizedZ), animated: false)
+            self.zLabel.text = String(z.truncate(places: self.lengthOfDoubles))
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        motionManager.accelerometerUpdateInterval = self.sampleSpeed
         motionManager.startAccelerometerUpdates(to: OperationQueue.main) { accelerometerData, error in
-            if let x = accelerometerData?.acceleration.x {
-                self.xValue.text = String(describing: x)
-                self.x = x
+            if let acc = accelerometerData?.acceleration {
+                self.x = acc.x
+                self.y = acc.y
+                self.z = acc.z
             }
-            
-            if let y = accelerometerData?.acceleration.y {
-                self.yValue.text = String(describing: y)
-                self.y = y
-            }
-            
-            if let z = accelerometerData?.acceleration.z {
-                self.zValue.text = String(describing: z)
-                self.z = z
-            }
-            
-            self.barChartUpdate()
         }
-        
-    }
-    
-    func barChartUpdate () {
-        let xEntry = BarChartDataEntry(x: 1.0, y: self.x)
-        let dataSetX = BarChartDataSet(values: [xEntry], label: "X")
-        dataSetX.valueColors = [UIColor.red]
-        dataSetX.colors = [UIColor.red]
-        
-        let yEntry = BarChartDataEntry(x: 2.0, y: self.y)
-        let dataSetY = BarChartDataSet(values: [yEntry], label: "Y")
-        dataSetY.valueColors = [UIColor.green]
-        dataSetY.colors = [UIColor.green]
-        
-        let zEntry = BarChartDataEntry(x: 3.0, y: self.z)
-        let dataSetZ = BarChartDataSet(values: [zEntry], label: "Z")
-        dataSetZ.valueColors = [UIColor.blue]
-        dataSetZ.colors = [UIColor.blue]
-        
-        let data = BarChartData(dataSets: [dataSetX, dataSetY, dataSetZ])
-        barChartView.data = data
-        barChartView.chartDescription?.text = "Accelerator Values"
-        
-        barChartView.notifyDataSetChanged()
     }
 
 }
 
+func normalizeValue(val : Double) -> Double {
+    return (val + 1.0) / 2.0;
+}
+
+extension Double {
+    func truncate(places : Int) -> Double {
+        return Double(floor(pow(10.0, Double(places)) * self)/pow(10.0, Double(places)))
+    }
+}
